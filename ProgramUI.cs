3925b7fb771026476234675d824aa094eb976a4c
Jeffrey Hackett <Jeffth92@gmail.com>
2021-06-26 15:32:29 -0400

    initializing and cooperating

---
+++ ProgramUI.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GroupProject
+{
+    public class ProgramUI
+    {
+        public enum Item { sword, TreasureKey };
+        public List<Item> inventory = new List<Item>();
+
+        Dictionary<string, Room> Rooms = new Dictionary<string, Room>
+        {
+            {"Treasury" treasury },
+            {"Armory" armory },
+            {"Cell" cell },
+            {"Hall" hall },
+        };
+
+        public void Run()
+        {
+            Rooms currentRoom = treasury;
+            Console.Clear(); //if we make a title card, put it in this bit
+            Console.WriteLine("You have snuck into the dungeon in search of treasure.\n" +
+                               "and have found The Treasury at last!");
+        }
+    }
+}
