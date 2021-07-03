2a72b468891546a173ab4c4a5dcccf2c8aec9566
Jeffrey Hackett <Jeffth92@gmail.com>
2021-07-03 16:58:49 -0400

    MVP

--- ProgramUI.cs
+++ ProgramUI.cs
@@ -9,7 +9,7 @@ namespace GroupProject
 {
     public class ProgramUI
     {
-        public enum Item { Sword, TreasureKey, HallKey, Mirror }; //mirror vs Gorgon item get image via ascii
+        public enum Item { Sword, TreasureKey, HallKey, Mirror }; //text
         public List<Item> inventory = new List<Item>();
 
         readonly Dictionary<string, Room> Rooms = new Dictionary<string, Room>
@@ -29,30 +29,27 @@ namespace GroupProject
         public void Run()
         {
             Room currentRoom = treasury;
-            Console.Clear(); //if we make a title card, put it in this bit
             Console.WriteLine("You have snuck into the dungeon in search of treasure\n" +
                                "and have found The Dungeon's wealthy Treasury at last!\n" +
                                "The chest is far too heavy to be moved, it seems you'll\n" +
                                "need the right key in hand before you leave with the riches.\n" +
                                 "------------------------------------------------------\n" +
                                 "Press any key to start");
-            Console.ReadLine();
+            Console.ReadKey();
+            Console.Clear();
             bool living = true;
             while (living)
             {
-                
-                Console.Clear();
                 Console.WriteLine(currentRoom.Splash);
                 foreach (Item item in currentRoom.Items)
                 {
                     Console.WriteLine($"there is a {item} in this room.");
                 }
-                Console.ReadLine();
                 string command = Console.ReadLine().ToLower();
                 bool foundExit = false;
-                if (command.StartsWith("go ") || command.StartsWith("move to ") || command.StartsWith("go to ") || command.StartsWith("enter ")) //go move enter: nsew considered at later time
+                if (command.StartsWith("go ") || command.StartsWith("move to ") || command.StartsWith("go to ") || command.StartsWith("enter "))
                 {
-                    foreach (string exit in currentRoom.Exits) //room connection check
+                    foreach (string exit in currentRoom.Exits)
                     {
                         if (command.Contains(exit) &&
                         Rooms.ContainsKey(exit))
@@ -70,33 +67,30 @@ namespace GroupProject
                 else if (command.StartsWith("get ") || command.StartsWith("grab ") || command.StartsWith("take ") || command.StartsWith("pick up "))
                 {
                     bool foundItem = false;
+                    string eventMessage = "You can't find one.";
                     foreach (Item item in currentRoom.Items)
                     {
                         if (!foundItem && command.Contains(item.ToString().ToLower()))
                         {
-                            Console.WriteLine($"You got {item}"
-                                + " try using it!"
-                                + " Press any key to continue..");
-                            currentRoom.RemoveItem(item);
                             inventory.Add(item);
+                            currentRoom.RemoveItem(item);
                             foundItem = true;
-                            Console.ReadKey();
+                            foreach(Event roomEvent in currentRoom.Events)
+                            {
+                                if (roomEvent.EventResult.Type == Result.ResultType.GetItem)
+                                {
+                                    eventMessage = roomEvent.EventResult.ResultMessage;
+                                }
+                            }
                             break;
                         }
                     }
-                    if (!foundItem)
-                    {
-                        Console.WriteLine("You can't find one.");
-                    }
-                    foreach (Item item in currentRoom.Items)
-                    {
-                        Console.WriteLine($"You see one {item}.");
-                    }
+                    Console.WriteLine(eventMessage);
                 }
                 else if (command.StartsWith("use ") || command.StartsWith("activate ") || command.StartsWith("try ") ||
                       (inventory.Contains(Item.Sword) && command.StartsWith("swing sword")) ||
                       (inventory.Contains(Item.TreasureKey) && command.StartsWith("turn ")) ||
-                      (inventory.Contains(Item.HallKey) && command.StartsWith("turn "))) //use activate ||swing sword execption? turn key (huge possibility of making a good text-parsing job.)
+                      (inventory.Contains(Item.HallKey) && command.StartsWith("turn ")))
                 {
                     string eventMessage = "You try, but nothing happens.";
                     foreach (Event roomEvent in currentRoom.Events)
@@ -124,10 +118,26 @@ namespace GroupProject
                 }
                 else
                 {
-                    Console.WriteLine("Hint: use GO, GET, or USE. similar words might work."); //cant perform "exiting else" general case error message
+                    Console.WriteLine("Hint: use GO, GET, or USE. similar words might work.");
                 }
+                //DEATH CHECKS
+                if (currentRoom == hall && command.Contains("sword"))   //DEATH CHECK Medusa
+                {
+                    living = false;
+                    Console.Clear();
+                    Console.WriteLine("You've Perished, Play again? Y/N");
+                    string PlayAgain = Console.ReadLine();
+                    if (PlayAgain.ToLower() == "y")
+                    {
+                        Run();
+                    }
+                }
+                //cleaning screen
+                Console.WriteLine("---------------------------------------------\n" + "Press any key to continue...");
+                Console.ReadKey();
+                Console.Clear();
             }
-        }  // Room geration here and non-random, hard-set outside of the while loop. next level from inside the chest?
+        }
 
         public static Room treasury = new Room(
             "You enter the treasury, as described here.\n" +
@@ -138,7 +148,7 @@ namespace GroupProject
                 new Event(
                 "treasurekey",
                 EventType.Use,
-                new Result("goal", "You open the path downward into the Chest!")
+                new Result("treasurechest", "You open the path downward into the Chest!")
                 ),
                 new Event(
                 "mirror",
@@ -174,14 +184,14 @@ namespace GroupProject
         );
 
         public static Room hall = new Room(
-           "Hall Text",
+           "Hall Text: You are aware of a Gorgon somewhere in this room.",
            new List<string> { "armory", "hiddenchamber" },
            new List<Item> { },
            new List<Event> {
                 new Event(
                 "sword",
                 EventType.Use,
-                new Result("Looking at your foe, You realize your mistake.")
+                new Result("Looking at your foe, You realize your mistake! You've been petrified.")
                 ),
                 new Event(
                 "mirror",
@@ -192,7 +202,7 @@ namespace GroupProject
        );
 
         public static Room hiddenchamber = new Room(
-            "Secret room text",
+            "THis small room is shaped so you cannot go back. You see that a tunnel might lead to the Treasury!",
             new List<string> { "treasury" },
             new List<Item> { Item.TreasureKey },
             new List<Event> { 
@@ -205,7 +215,7 @@ namespace GroupProject
         );
 
         public static Room treasurechest = new Room(
-            "You've beaten the demo.\n +" +
+            "You've beaten the demo.\n" +
             "THANKS FOR PLAYING!",
             new List<string> { "treasury" },
             new List<Item> { },
